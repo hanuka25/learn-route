5a24a69401c62d3f643f9578ded82e7cb7b2c8e6
Michelle Christina <michellechristina89@gmail.com>
2022-09-09 20:49:10 +0700

    Menambahkan rute /admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    $nama = "Nama";
+    echo "<h1>Ini Halaman Admin $nama</h1>";
+});
